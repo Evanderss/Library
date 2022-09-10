ec8640eed0c39c559ab2ea8a85ec9b57ba32009e
Evanderss <evanderramirez150@gmail.com>
2022-09-09 22:14:25 -0500

    Creando la funcion delete y __str__

--- libreria/models.py
+++ libreria/models.py
@@ -6,3 +6,11 @@ class book(models.Model):
     title = models.CharField(max_length=100, verbose_name="Title")
     description = models.TextField(verbose_name="Description", null=True)
     image = models.ImageField(upload_to="images/", verbose_name="Image", null=True)
+
+    def __str__(self):
+        fila = "Title: " + self.title + "-" + "Description: " + self.description
+        return fila
+    
+    def delete(self, using=None, keep_parents=False):
+        self.image.storage.delete(self.image.name)
+        super().delete()
