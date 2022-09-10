4101212ae4530128e3380c1270a0da03fc8c4adb
Evanderss <evanderramirez150@gmail.com>
2022-09-09 22:49:16 -0500

    Consultando los libros desde admin y moestrando en views

--- libreria/views.py
+++ libreria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import book
 # Create your views here.
 def homepage(request):
     return render(request, "pages/homepage.html")
@@ -8,7 +9,8 @@ def about(request):
     return render(request, "pages/about.html")
 
 def books(request):
-    return render(request, "books/index.html")
+    books = book.objects.all()
+    return render(request, "books/index.html", {"books" : books})
 
 def create(request):
     return render(request, "books/create.html")
