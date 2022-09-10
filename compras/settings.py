542b9a33bc1b46d4dd6fcb7debdb2b664df57cff
Evanderss <evanderramirez150@gmail.com>
2022-09-09 19:46:36 -0500

    conectando con base de datos

--- compras/settings.py
+++ compras/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'compras.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'libreria',
+        'USER': 'postgres',
+        'PASSWORD': 'evanderss',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
