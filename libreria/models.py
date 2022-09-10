de36f503d503fc117c5d6338939a1a9bfd4875bb
Evanderss <evanderramirez150@gmail.com>
2022-09-09 20:27:43 -0500

    creando los modelos para la tabla en postgresql

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, verbose_name="Title")
+    description = models.TextField(verbose_name="Description", null=True)
+    image = models.ImageField(upload_to="images/", verbose_name="Image", null=True)
