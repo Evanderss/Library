3a52c37c97d5b5528056476b4800aeca189969b9
Evanderss <evanderramirez150@gmail.com>
2022-09-09 23:21:41 -0500

    Haciendo la url para visualizar las images, modificando seetingsy urls

--- libreria/urls.py
+++ libreria/urls.py
@@ -2,10 +2,12 @@ from struct import pack
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path("", views.homepage, name="homepage"), 
     path("about", views.about, name="about"),
     path("books", views.books, name="books"),
     path("books/create", views.create, name="create"),
     path("books/edit", views.edit, name="edit")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
